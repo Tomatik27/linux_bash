ea09f62f5daf85a60773dacae7bec6938aa35b37
Tomatik27 <Tematik27@yandex.ru>
2025-09-27 10:27:47 +0300

    task_4
    creating history file

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
 using namespace std;
@@ -7,14 +8,27 @@ int main() {
   cout << unitbuf;
   cerr << unitbuf;
 
+  string hist = "kubsh_history.txt";
+  ofstream F(hist, ios::app);
+  
   cout << "$ ";
 
   string input;
   while (getline(cin, input)) {
+
+    F << '$' << input << '\n';
+    F.flush();
+    
     cout << input << endl;
     cout << "$ ";
+    
     if (input == "\\q")
       break;
+    
+      if (input.empty())
+      continue;
+
+
   }
   return 0;
 }
