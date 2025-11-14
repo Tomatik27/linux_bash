abaadb5ec1c8d38e037829d39519ff50115dc2c1
Tomatik27 <Tematik27@yandex.ru>
2025-11-14 19:29:05 +0300

    safe task5

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
 #include <cstdlib>
 
 using namespace std;
@@ -13,69 +12,77 @@ int main() {
   string hist = "kubsh_history.txt";
   ofstream F(hist, ios::app);
   
+  cout << "$ ";
 
   string input;
   while (getline(cin, input)) {
-
-    while (input.at(0) == ' ')
+    // Безопасное удаление начальных пробелов
+    while (!input.empty() && input[0] == ' ') {
       input.erase(0, 1);
+    }
 
     F << '$' << input << '\n';
     F.flush();
     
     if (input == "\\q")
-    break;
+      break;
     
-    if (input.empty())
-    continue;
-    
-    if (input.find("debug") == 0){
-      input.erase(0, 5);
-      while (input.at(0) == ' ') input.erase(0, 1);
-
-      if ((input.at(0) == '"') || (input.at(0) == '\'')) {input.erase(0, 1); input.erase(input.length()-1,input.length());}
-      cout << input.substr(0, input.length()) << '\n';
+    if (input.empty()) {
+      cout << "$ ";
+      continue;
     }
-
-    else if (input.find("\\e") == 0)
-    {
+    
+    // Обработка debug
+    if (input.find("debug") == 0) {
+      input = input.substr(5); // -debug
+      
+      // Пробелы
+      while (!input.empty() && input[0] == ' ') {
+        input.erase(0, 1);
+      }
+      
+      // Кавычки 
+      if (!input.empty()) {
+        if ((input[0] == '"' && input[input.length()-1] == '"') || 
+            (input[0] == '\'' && input[input.length()-1] == '\'')) {
+          input = input.substr(1, input.length()-2);
+        }
+      }
+      cout << input << '\n';
+    }
+    
+    else if (input.find("\\e") == 0) {
       size_t pos = input.find("\\e") + 3;
 
-      if (pos < input.length()) 
-      {
+      if (pos < input.length()) {
         string var_name = input.substr(pos);
         
         if (!var_name.empty() && var_name[0] == '$') 
           var_name = var_name.substr(1);
         
         const char* env_value = getenv(var_name.c_str());
-        if (env_value != nullptr) 
-        {
+        if (env_value != nullptr) {
           string value = env_value;
           size_t start = 0;
           size_t end = value.find(':');
         
-          while (end != string::npos) 
-          {
+          while (end != string::npos) {
             cout << value.substr(start, end - start) << '\n';
             start = end + 1;
             end = value.find(':', start);
           }
-
           cout << value.substr(start) << '\n';
-        }
-         else
+        } else {
           cout << "Environment variable '" << var_name << "' not found" << '\n';
-
-        } 
-
-      else cout << "Usage: \\e $VARIABLE" << '\n';
-
+        }
+      } else {
+        cout << "Usage: \\e $VARIABLE" << '\n';
+      }
+    } else {
+      cout << input << ": command not found" << '\n';
     }
     
-    else cout << input << ": command not found" << '\n';
-    
-
+    cout << "$ ";
   }
-
+  return 0;
 }
